a699db7e415f3004361481d6499ed248927a4d7e
Tyler Guillen <tyguillen@gmail.com>
2016-03-07 10:38:26 -0500

    add scaffolds for pages and dynamically generated routes

--- tyler_guillen_website.rb
+++ tyler_guillen_website.rb
@@ -3,6 +3,20 @@ require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
 require "tilt/erubis"
 
+JOBS = { webdev: "Web Developer",
+         photo: "Photographer",
+         phil: "Philosopher" }.freeze
+
 get '/' do
   erb :splash, layout: :splash_layout
+end
+
+get '/:role' do
+  @role = params[:role].to_sym
+
+  if JOBS.keys.include? @role
+    erb @role, layout: :main_layout
+  else
+    redirect '/'
+  end
 end
