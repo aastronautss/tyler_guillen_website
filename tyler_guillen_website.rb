22776b60cd8b6a2a0ee68ac0d19622250225a606
Tyler Guillen <tyguillen@gmail.com>
2016-03-07 18:28:20 -0500

    make page generating more dynamic

--- tyler_guillen_website.rb
+++ tyler_guillen_website.rb
@@ -7,8 +7,44 @@ JOBS = { webdev: "Web Developer",
          photo: "Photographer",
          phil: "Philosopher" }.freeze
 
+def nonexistent_role?
+  !JOBS.keys.include? @role
+end
+
+def nonexistent_subpage?
+  !File.exist? "views/#{@subpage}.erb"
+end
+
+def render_role_subpage
+  if nonexistent_role?
+    redirect '/'
+  elsif nonexistent_subpage?
+    status 404
+  else
+    erb @subpage, layout: :main_layout
+  end
+end
+
+helpers do
+  def webdev_links
+    { "/webdev/about" => "About",
+      "/webdev/contact" => "Contact" }
+  end
+
+  def photo_links
+    { "/photo/portfolio" => "Portfolio",
+      "/photo/about" => "About",
+      "/photo/contact" => "Contact" }
+  end
+
+  def phil_links
+    { "/phil/about" => "About",
+      "/phil/contact" => "Contact" }
+  end
+end
+
 not_found do
-  redirect '/'
+  @role ? erb("#{@role}_not_found".to_sym, layout: :main_layout) : redirect('/')
 end
 
 get '/' do
@@ -23,4 +59,11 @@ get '/:role' do
   else
     redirect '/'
   end
+end
+
+get '/:role/:page' do
+  @role = params[:role].to_sym
+  @subpage = (params[:role] + "_" + params[:page]).to_sym
+
+  render_role_subpage
 end
