a5955d9cfc764c08c0bfeb38428785c5d027299a
Tyler Guillen <tyguillen@gmail.com>
2017-02-20 21:49:54 -0500

    restructure nav and DRY up some page stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,29 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
+
+  PAGE_METADATA = {
+    'home' => {
+      title: 'Home',
+    },
+
+    'webdev' => {
+      title: 'Web Development'
+    },
+
+    'photo' => {
+      title: 'Photography'
+    },
+
+    'contact' => {
+      title: 'Contact'
+    }
+  }.freeze
+
+  before_action :set_page_metadata
+
+  private
+
+  def set_page_metadata
+    @pages = PAGE_METADATA
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#show', id: 'home'
+
+  get '/*id' => 'pages#show', as: :page, format: false
 end
